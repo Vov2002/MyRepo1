5cba6ff763aa05fefc965831cd8152fabc8d596a
Vov2002 <Vladimiy2002@mail.ru>
2023-01-31 19:26:42 +0300

    Dz4

---
+++ js/main.js
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+//Задание 1
+let a = 8
+let b = 10
+if(a + b <= 15){
+    console.log('НЕТ')
+}
+else{
+    console.log('ДА')
+}
+
+//Задание 2
+let a2 = 15
+let b2 = '15'
+if(a2 === b2 ){
+    console.log(a2 === b2)
+}
+else {
+    console.log(a2 === b2)
+=======
+let number = prompt('Введите число')
+let v = document.querySelector('.out')
+if(number > 100){
+    v.innerHTML = number
+}
+else{
+    console.log(number)
+>>>>>>> 78884aab92b72817a24c0376242e64d4e64cea39
+}
